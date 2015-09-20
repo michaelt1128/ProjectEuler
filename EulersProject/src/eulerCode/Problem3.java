72425e78986cfe1115dbfefb07a0785ba49f815b
michaelt1128 <michael.turner1128@gmail.com>
2015-09-20 15:56:16 -0400

    Problem3 Complete

---
+++ EulersProject/src/eulerCode/Problem3.java
@@ -0,0 +1,35 @@
+package eulerCode;
+
+public class Problem3 {
+	public static void main(String[] args)
+	{
+		long num = 600851475143L;
+		long i=2L;
+		boolean hasFound = false;
+		while (hasFound == false)
+		{
+			if((num%i==0) && isPrime(num/i))
+			{
+				long value = num/i;
+				System.out.println("maybe" + value);
+				hasFound = true;
+			}
+			i++;			
+		}
+	}
+	public static boolean isPrime(long x)
+	{
+		boolean prime = true;
+		long y =2L;		
+		while (y < (x/2))
+		{
+			if(x%y==0)
+			{
+				prime = false;
+				y = x;
+			}
+			y++;
+		}
+		return prime;
+	}
+}

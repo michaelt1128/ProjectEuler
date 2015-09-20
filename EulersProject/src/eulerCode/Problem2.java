bd02229abbd1310f84cd9fd012375f045ea7af27
michaelt1128 <michael.turner1128@gmail.com>
2015-09-20 15:19:19 -0400

    problem 2 done

--- EulersProject/src/eulerCode/Problem2.java
+++ EulersProject/src/eulerCode/Problem2.java
@@ -4,14 +4,18 @@ public class Problem2
 {
 	public static void main(String[] args)
 	{
-		int sum = 0;
 		int fibNumSum = 0;
 		int fibNum = 1;
 		int fibNumPrev = 1;
 		while (fibNum < 4000000)
 		{
 			fibNum = fibNum + fibNumPrev;
-			
+			fibNumPrev = fibNum - fibNumPrev;
+			if (fibNum%2==0)
+			{
+				fibNumSum += fibNum;	
+			}
+			System.out.println(fibNumSum);
 		}
 	}
 }

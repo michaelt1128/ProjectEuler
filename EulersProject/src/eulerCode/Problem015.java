d9fa8482d54ee313f37c2cd41cffbed9cc4baba5
michaelt1128 <michael.turner1128@gmail.com>
2015-09-22 23:18:42 -0400

    Problem 15 done
    this was difficult lol

---
+++ EulersProject/src/eulerCode/Problem015.java
@@ -0,0 +1,32 @@
+package eulerCode;
+
+import java.math.BigInteger;
+
+public class Problem015 {
+	public static void main(String[] args)
+	{
+		for(int len = 0; len <=20; len++)
+		{
+			int n = len;
+			int k = len;
+			System.out.println("binom: " + binomialCoefficient(n+k, n));
+		}
+	}
+	public static String factorial(int n) 
+	{
+	       BigInteger fact = new BigInteger("1");
+	       for (int i = 1; i <= n; i++) 
+	       {
+	           fact = fact.multiply(new BigInteger(i + ""));
+	       }
+	       return fact.toString();
+	   }
+	public static long binomialCoefficient(int nplusk, int k)
+	{
+		BigInteger a = new BigInteger(factorial(nplusk));
+		BigInteger b = new BigInteger(factorial(nplusk-k));
+		BigInteger c = new BigInteger(factorial(k));
+		BigInteger binom = a.divide(c.multiply(b));
+		return binom.longValue();
+	}
+}

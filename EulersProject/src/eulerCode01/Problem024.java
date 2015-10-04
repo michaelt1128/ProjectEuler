1193acbb935cbbd67aadf697899c4bf63b8e528d
michaelt1128 <michael.turner1128@gmail.com>
2015-10-03 21:29:11 -0400

    Problem 24 complete

---
+++ EulersProject/src/eulerCode01/Problem024.java
@@ -0,0 +1,44 @@
+package eulerCode01;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Problem024 {
+
+	public static void main(String[] args) {
+		int[] array = {0,1,2,3,4,5,6,7,8,9};
+		int num = 1;
+		while (num<1000000){
+			nextPermutation(array);
+			num++;
+		}
+		System.out.println(num);
+		System.out.println(Arrays.toString(array));
+	}
+	public static boolean nextPermutation(int[] array) {
+		int i = array.length - 1;
+		while(i > 0 && array[i-1] >= array[i]) {
+			i--;
+		}
+		if (i<=0) {
+			return false;
+		}
+		int j = array.length - 1;
+		while (array[j] <= array[i-1]) {
+			j--;
+		}
+		int temp = array[i-1];
+		array[i-1] = array[j];
+		array[j] = temp;
+		j = array.length - 1;
+		while (i < j) {
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+			j--;
+		}
+		return true;
+	}
+}

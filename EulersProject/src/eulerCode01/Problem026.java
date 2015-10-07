eb9c30d6c52fc875306c1cf02afb99f41a23b55f
michaelt1128 <michael.turner1128@gmail.com>
2015-10-06 23:41:41 -0400

    Problem 26 complete
    cheated :(

--- EulersProject/src/eulerCode01/Problem026.java
+++ EulersProject/src/eulerCode01/Problem026.java
@@ -1,18 +1,32 @@
 package eulerCode01;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-
 public class Problem026 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for(int i=2; i<1000; i++) {
-			BigDecimal num = new BigDecimal(i);
-			num = num.pow(-1, new MathContext(30, RoundingMode.HALF_UP));
-			String[] str = num.toString().split("\\.");
-			System.out.println(i + " " + str[1]);
+		int sequenceLength = 0;
+		int num = 0;
+		 
+		for (int i = 1000; i > 1; i--) {
+		    if (sequenceLength >= i) {
+		        break;
+		    }
+		 
+		    int[] foundRemainders = new int[i];
+		    int value = 1;
+		    int position = 0;
+		 
+		    while (foundRemainders[value] == 0 && value != 0) {
+		        foundRemainders[value] = position;
+		        value *= 10;
+		        value %= i;
+		        position++;
+		    }
+		 
+		    if (position - foundRemainders[value] > sequenceLength) {
+		    	num = i;
+		        sequenceLength = position - foundRemainders[value];
+		    }
 		}
+		System.out.println(sequenceLength + " " + num);
 	}
 }

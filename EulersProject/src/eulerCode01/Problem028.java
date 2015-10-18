575b9c8eb5f9c58e1d899819cb4a9088696c8fa2
michaelt1128 <michael.turner1128@gmail.com>
2015-10-18 00:17:49 -0400

    Problem 28 complete

---
+++ EulersProject/src/eulerCode01/Problem028.java
@@ -0,0 +1,21 @@
+package eulerCode01;
+
+public class Problem028 {
+
+	public static void main(String[] args) {
+		Problem028 p = new Problem028();
+		int sum = 1;
+		for(int i = 3; i <=1001; i=i+2) {
+			sum+=p.prob28(i);
+			System.out.println(sum);
+		}
+		
+	}
+	public int prob28(int n) {
+		int answer = 0;
+		int s = n*n; 
+		answer = 4 * s - 6 * (n-1);
+		
+		return answer;
+	}
+}
